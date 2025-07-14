e8c2be8fc91d154faade307fe46c692f31edbb09
Antonio Sarmiento <antoniosaryon155@gmail.com>
2025-07-14 06:48:12 -0700

    Adding Base Animation and starting LightMechanic

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    #region Props
+
+    [SerializeField] private Button btnLightSwitch;
+    [SerializeField] private GameObject LightPanel;
+
+    #endregion
+
+    //----------------------------------------------------------------------------------
+
+    #region methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TurnOffLight()
+    {
+
+    }
+
+    public void TurnOnLight()
+    {
+
+    }
+
+    #endregion
+}
